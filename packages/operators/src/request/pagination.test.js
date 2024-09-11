f214cd143c0dd417798d49faca41bbbc8b55cc11
StephanGerbeth <stephan.gerbeth@gmail.com>
2024-09-11 17:03:12 +0200

    fix(test): corrected outline

---
+++ packages/operators/src/request/pagination.test.js
@@ -0,0 +1,9 @@
+describe('paginated data', function () {
+  it('single response', async function () {
+    console.log('YEAH');
+  });
+
+  it('paginated response', async function () {
+    console.log('YEAH');
+  });
+});
--- packages/operators/src/request/retry.test.js
+++ packages/operators/src/request/retry.test.js
@@ -1,5 +1,17 @@
-describe('User', function () {
-  it('should save without error', async function () {
+describe('request retry', function () {
+  it('interrupted connection 1000ms', async function () {
+    console.log('YEAH');
+    //expect(true).to.be.true;
+
+    const req = new URL('https://dummyjson.com/products');
+    req.searchParams.append('limit', 10);
+    req.searchParams.append('skip', 5);
+    req.searchParams.append('select', 'key1,key2,key3');
+    const resp = await fetch(req);
+    console.log(await resp.json());
+  });
+
+  it('interrupted connection 10000ms', async function () {
     console.log('YEAH');
     //expect(true).to.be.true;
 
