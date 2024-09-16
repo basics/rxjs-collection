b6c1d12af89dfec98b9a19dc6af818299eae362c
StephanGerbeth <stephan.gerbeth@gmail.com>
2024-09-16 09:52:30 +0200

    fix(vitest): added lcov as coverage reporter

--- vitest.config.js
+++ vitest.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'clover'],
+      reporter: ['text', 'json', 'lcov'],
       reportsDirectory: './coverage',
       exclude: [
         ...configDefaults.exclude,
