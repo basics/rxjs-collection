7df804acd9df3b0013dcc5b92a285dcf352a843a
StephanGerbeth <stephan.gerbeth@gmail.com>
2024-11-28 22:26:01 +0100

    fix(vitest): better config

--- packages/operators/vitest.config.js
+++ packages/operators/vitest.config.js
@@ -1,4 +1,4 @@
-import { defineProject, configDefaults } from 'vitest/config';
+import { defineProject } from 'vitest/config';
 
 export default defineProject({
   cacheDir: '../.cache/vitest-operators',
--- vitest.config.js
+++ vitest.config.js
@@ -3,7 +3,6 @@ import { defineConfig, configDefaults } from 'vitest/config';
 export default defineConfig({
   test: {
     cacheDir: './.cache/vitest',
-
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'lcov', 'html'],
