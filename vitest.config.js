df028a70547be7095e3cab9663dd9b45d29304e9
StephanGerbeth <stephan.gerbeth@gmail.com>
2024-12-06 10:44:05 +0100

    fix(operators): log test completed

--- vitest.config.js
+++ vitest.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
         'commitlint.config.js',
         'eslint.ignores.js',
         'vitest.workspace.js',
+        './coverage/**/*.*',
         './packages/mocks/',
         './packages/*/src/index.js'
       ]
