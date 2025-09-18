83b771b4f716a2f70af40a6c54027a40b20e51b5
Thorn Walli <lammpee@gmail.com>
2025-09-18 08:08:53 +0200

    fix: use globalThis for event listeners in connectionObservable

--- packages/observables/src/dom/window.ts
+++ packages/observables/src/dom/window.ts
@@ -2,8 +2,8 @@ import { fromEvent, merge, of, map, shareReplay } from 'rxjs';
 
 export const connectionObservable = merge(
   of(null),
-  fromEvent(global, 'online'),
-  fromEvent(global, 'offline')
+  fromEvent(globalThis, 'online'),
+  fromEvent(globalThis, 'offline')
 ).pipe(
   map(() => navigator.onLine),
   shareReplay(1)
